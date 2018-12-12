4e49eac7571eb9d9f0f465665d433c13991b662f
Jason Michael <michael.jason@outlook.com>
2018-12-11 23:08:14 -0600

    added hacky shooting

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
+window.addEventListener('load', function () {
+    updateGameSize();
+});
 
+window.addEventListener('resize', function () {
+    updateGameSize();
+});
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     physics: {
         default: 'arcade',
         arcade: {
@@ -21,8 +27,18 @@ const config = {
 const game = new Phaser.Game(config);
 
 let player;
-let playerSpeed = 300;
+let playerSpeed = 350;
+let bullet;
+let pointer;
+let fireRate = 200;
+let nextFire = 0;
 
+let gameWidth = 500;
+let gameHeight = 500;
+
+let shouldUpdateRotation = false;
+
+//--------------------------------------
 
 function preload() {
     this.load.setBaseURL('http://labs.phaser.io');
@@ -30,12 +46,16 @@ function preload() {
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png');
+
+    this.load.image('bullet', 'assets/particles/logo.png');
+
 }
 
-let pointer;
-function create() {
-    this.add.image(400, 300, 'sky');
 
+function create() {
+    // this.add.image(400, 300, 'sky');
+
+    // PLAYER SETUP
     player = this.physics.add.image(400, 100, 'logo');
     player.setBounce(0.2);
     player.setScale(0.5, 0.5)
@@ -49,22 +69,63 @@ function create() {
 
     // LOOK-AT ROTATION
     this.input.on('pointermove', function (_pointer) {
+        shouldUpdateRotation = true;
         pointer = _pointer;
     });
+
 }
+
 
 function update() {
 
     addMovementInput();
     updateRotation();
 
+    if (game.input.activePointer.isDown) {
+        fire(this);
+    }
+
+    this.physics.world.setBounds(0, 0, gameWidth, gameHeight, true, true, true, true);
 }
 
-function updateRotation () {
-    player.rotation = Phaser.Math.Angle.Between(player.x, player.y, pointer.worldX, pointer.worldY);
+//--------------------------------------
+function fire (self) {
+    if (game.loop.time > nextFire) {
+        nextFire = game.loop.time + fireRate;
+
+        let particles = self.add.particles('red');
+
+        let emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
+
+
+        bullet = self.physics.add.image(player.x, player.y, 'logo');
+        bullet.setScale(.1, .25);
+        bullet.rotation = Phaser.Math.Angle.Between(player.x, player.y, pointer.worldX, pointer.worldY);
+        emitter.startFollow(bullet);
+
+        self.physics.moveTo(bullet, pointer.x, pointer.y, 2000)
+    }
 }
 
-function addMovementInput () {
+function updateGameSize () {
+    gameWidth = window.innerWidth;
+    gameHeight = window.innerHeight;
+    game.config.width = gameWidth;
+    game.config.height = gameHeight;
+    game.resize(gameWidth, gameHeight); // ? Needed?
+}
+
+function updateRotation() {
+    if (shouldUpdateRotation) {
+        player.rotation = Phaser.Math.Angle.Between(player.x, player.y, pointer.worldX, pointer.worldY);
+    }
+}
+
+function addMovementInput() {
 
     // UP and LEFT
     if (WKey.isDown && AKey.isDown) {
